868d1c72072bad006de6ca0b6d78f7de378b5265
Christophe Paque <80208557+keyxmare@users.noreply.github.com>
2025-08-31 11:35:23 +0200

    Add descriptions to alias listing

--- aliases/git.sh
+++ aliases/git.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 # Git related aliases
+
+alias_git_prune_branches_desc='fetch all remote branches, check out main, and delete all other local branches'
+alias_aliases_desc='list all defined aliases with descriptions'
+
+_aliases() {
+  alias -p | while IFS= read -r line; do
+    name=$(printf '%s' "$line" | sed -n "s/^alias \([^=]*\)=.*/\1/p")
+    case "$name" in
+      git-prune-branches)
+        desc=$alias_git_prune_branches_desc
+        ;;
+      aliases)
+        desc=$alias_aliases_desc
+        ;;
+      *)
+        desc=''
+        ;;
+    esac
+    printf '%s # %s\n' "$line" "$desc"
+  done
+}
+
 alias git-prune-branches='git fetch --all && git checkout main && git branch | grep -v "main" | xargs git branch -D'
+alias aliases='_aliases'
+
